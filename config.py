adfb9390aac19434360e1b4e1a6a0079ef4227af
lichiareghu <90550296+lichiareghu@users.noreply.github.com>
2024-04-21 07:25:29 +0100

    Adding Web Socket connection

--- config.py
+++ config.py
@@ -8,7 +8,7 @@ load_dotenv(find_dotenv())
 class Config:
 
     # Set Slack API credentials
-    SLACK_BOT_TOKEN = os.environ["SLACK_BOT_TOKEN"]
+    SLACK_BOT_TOKEN = os.environ["SLACK_BOT_TOKEN_SOCKET"]
     SLACK_SIGNING_SECRET = os.environ["SLACK_SIGNING_SECRET"]
     SLACK_BOT_USER_ID = os.environ["SLACK_BOT_USER_ID"]
     OPENAI_API_KEY = os.environ["OPENAI_API_KEY"]
